e5803ebc42e5ddda26942f9b3640077936a13269
Dao Kim Duong <daokimduong322@gmail.com>
2022-06-13 14:25:07 +0700

    Chapter 6: Modeling User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   private
-    def set_locale
-      I18n.locale = params[:locale] || I18n.default_locale
-    end
-    def default_url_options
-      {locale: I18n.locale}
-    end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end

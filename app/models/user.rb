21ba0c880605d793270afe64be25aa4aab609a7e
Dao Kim Duong <daokimduong322@gmail.com>
2022-06-13 14:25:07 +0700

    Chapter 6: Modeling User

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save :downcase_email
+
+  validates :email, presence: true, length: {minimum: 20, maximum: 40},
+  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  validates :name, presence: true, length: {maximum: 10}
+  has_secure_password
+
+  validates :password, presence: true, length: {minimum: 6}, if: :password
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
+end
